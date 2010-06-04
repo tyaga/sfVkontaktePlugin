bb1da12ef65bb678299fed51d33239b7ff6b6a28
Alexey Tyagunov <atyaga@gmail.com>
2010-06-05 02:17:57 +0400

    remomve unnesesary

--- modules/sfVkontakteFetch/templates/_init.php
+++ modules/sfVkontakteFetch/templates/_init.php
@@ -1,7 +1,6 @@
 <? if (!is_null($sf_user->id) && $sf_user->id): ?>
 
 <script type="text/javascript" language="javascript">
-	App.encrypted_key = '<?=sfConfig::get('sf_vkontakte_encrypted_key')?>';
 	App.profile_fields = '<?=implode(',', array_keys(sfConfig::get('app_vkontakte_profile_fields')))?>';
 	App.fetch_url = '<?=url_for('@sf_vkontakte_fetch_profiles'); ?>';
 	App.upload_photo_url = '<?=url_for('@sf_vkontakte_upload_photo'); ?>';
