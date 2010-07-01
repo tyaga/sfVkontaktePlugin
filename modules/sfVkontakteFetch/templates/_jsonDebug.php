9c1427853775dec4e1c717eeeccd41c5bd7c89cb
tyaga <tyaga@tyaga-laptop.(none)>
2010-07-01 22:22:07 +0800

    add html to json debug layout

--- modules/sfVkontakteFetch/templates/_jsonDebug.php
+++ modules/sfVkontakteFetch/templates/_jsonDebug.php
@@ -1,9 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>JSON response</title>
+<link rel="shortcut icon" href="/favicon.ico" />
+</head>
+<body>
 data structure:<br>
-
 <pre>
-<?php print_r($data); ?>
+<? print_r($data); ?>
 </pre>
-
 json:<br>
-
-<?php echo $json;?>
+<?=$json;?>
+</body>
+</html>
